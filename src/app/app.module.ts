2377593c79f3b3e89cfaa8c4647ffbeb798eba0a
abuenoa <abuenoa@viewnext.com>
2020-05-12 14:01:01 +0200

    Modificaciones en los módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// RUTAS
+// Rutas
 import { APP_ROUTES } from './app.routes';
 
-import { PagesComponent } from './pages/pages.component';
+// Módulos
+import { PagesModule } from './pages/pages.module';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagesComponent,
-    DashboardComponent,
-    NopagefoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    Graficas1Component,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    // PagesModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
